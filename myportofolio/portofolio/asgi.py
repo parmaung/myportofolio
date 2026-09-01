10f448f5bb9559fae2e6bb5c72448d520ff2ad85
parmaung <muhammad.rafif54@ui.ac.id>
2026-09-02 03:57:04 +0700

    update

--- myportofolio/portofolio/asgi.py
+++ myportofolio/portofolio/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for myportofolio project.
+ASGI config for portofolio project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- myportofolio/portofolio/urls.py
+++ myportofolio/portofolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myportofolio project.
+URL configuration for portofolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
